bd18750b059983a13ffff1ba8c725eb89a09b134
Roman15122022 <mallrom1231@gmail.com>
2023-08-06 00:07:30 +0300

    some problems lesson 4

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import {Component, NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HousingLocationComponent} from "../housing-location/housing-location.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
+  `,
+  styleUrls: ['./home.component.css']
+})
+@NgModule({
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+})
+export class HomeComponent {
+
+}
