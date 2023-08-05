9398070d16d69ca87c7da5d6647da95ae1497890
Roman15122022 <mallrom1231@gmail.com>
2023-08-05 23:55:03 +0300

    lesson 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {HomeComponent} from "./home/home.component";
   ],
   imports: [
     BrowserModule,
-    HomeComponent
+    HomeComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
